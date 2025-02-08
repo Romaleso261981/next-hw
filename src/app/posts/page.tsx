126bf283ad57db230894091c6fa94aa74e1558de
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2025-02-08 10:05:05 +0200

    fix: fix the requests on the users posts pages

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,38 +1,11 @@
 import React from "react";
 import styles from "./posts.module.css";
 
-interface Post {
-  id: number;
-  name: string;
-  body: number;
-}
-
-// Функція для отримання даних
-async function fetchPosts(): Promise<Post[]> {
-  const res = await fetch("/api/cars");
-
-  return res.json();
-}
-
 // Компонент сторінки
 export default async function PostsPage() {
-  const posts = await fetchPosts();
-
   return (
     <div className={styles.container}>
       <h1 className={styles.mainTitle}>Posts</h1>
-      <ul className={styles.postsList}>
-        {posts.map(post =>
-          <li key={post.id} className={styles.post}>
-            <h2>
-              {post.name}
-            </h2>
-            <p>
-              Price: {post.body}
-            </p>
-          </li>
-        )}
-      </ul>
     </div>
   );
 }
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -2,40 +2,29 @@ import React from "react";
 
 import styles from "./users.module.css";
 
-interface Users {
-  id: number;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-  };
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-  email: string;
-  name: string;
-  phone: string;
-  username: string;
-  website: string;
-}
-
-// Функція для отримання даних
-async function fetchUsers(): Promise<Users[]> {
-  const res = await fetch("/api/cars");
-
-  return res.json();
-}
+// interface Users {
+//   id: number;
+//   address: {
+//     street: string;
+//     suite: string;
+//     city: string;
+//   };
+//   company: {
+//     name: string;
+//     catchPhrase: string;
+//     bs: string;
+//   };
+//   email: string;
+//   name: string;
+//   phone: string;
+//   username: string;
+//   website: string;
+// }
 
 export default async function UsersPage() {
-  const users = await fetchUsers();
-
   return (
     <div className={styles.container}>
-      <h1 className={styles.mainTitle}>
-        {JSON.stringify(users)}
-      </h1>
+      <h1 className={styles.mainTitle}>Users</h1>
       {/* <ul className={styles.usersList}>
         {users.map(user =>
           <li key={user.id} className={styles.user}>
